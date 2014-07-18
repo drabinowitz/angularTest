3fa92961e1cd844d2ca96874ea3770e74a9eba72
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-18 07:46:55 -0400

    filter testing

---
+++ app.js
@@ -0,0 +1,19 @@
+function myController($scope) {
+			$scope.messages = [
+        
+        {content: "What would Kitty-Meow-Meow do? #wwkmmd",
+				author: 'Peter Cellar',
+				views: 198,
+				published: '2009-02-03'},
+        
+				{content: "How's it going? Did you see that sunset?",
+				author: 'John Doe',
+				views: 190000,
+				published: 1288323623006},
+				
+				{content: "I wonder if Justin Bieber has many Twitter followers",
+				author: 'Jane Doe',
+				views: 1431,
+				published: '2014-04-01'}
+			]
+		}
