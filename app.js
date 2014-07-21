e79c2e27d318ff93a2ddf58ee140307f72fd080e
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-21 11:42:03 -0400

    reset

--- app.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp', [])
-	.constant('VERSION',1.1)
-	.run(function($rootScope,VERSION){
-		$rootScope.version = VERSION;
-	})
-	.controller('myCtrl',['$scope',function($scope){
-		$scope.version = 9.9;
-	}]);
