7a9456ea4e3c138ac9d29dc9d49d509a7c7482a6
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-24 14:41:07 -0400

    initialize

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-var myApp = angular.module('myApp',['ngAnimate']);
+angular.module('CORSDemo', [])
+	
+	.config(function($httpProvider){
+		
+		$httpProvider.defaults.useXDomain = true;
+	
+	})
+
+	.controller('ytCtrl', function($scope, $http, $sce){
+
+		var embedUrl = "http://www.youtube.com/embed/";
+
+		$scope.trustSrc = function(resultId){
+
+			return $sce.trustAsResourceUrl(embedUrl + resultId); 
+
+		}
+
+	});
