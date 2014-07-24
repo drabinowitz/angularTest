f2103c0ccb1711cd6e0df200d3c6136312860b04
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-24 14:48:34 -0400

    completed request

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-angular.module('CORSDemo', [])
+angular.module('CORSDemo', []).
 	
-	.config(function($httpProvider){
+	config(function($httpProvider){
 		
 		$httpProvider.defaults.useXDomain = true;
 	
-	})
+	}).
 
-	.controller('ytCtrl', function($scope, $http, $sce){
+	controller('ytCtrl', function($scope, $http, $sce){
 
 		var embedUrl = "http://www.youtube.com/embed/";
 
@@ -15,5 +15,47 @@ angular.module('CORSDemo', [])
 			return $sce.trustAsResourceUrl(embedUrl + resultId); 
 
 		}
+
+		$scope.searchYouTube = function(keyword){
+
+			$scope.keyword = keyword;
+
+			var url = "https://www.googleapis.com/youtube/v3/search";
+
+			var request = {
+
+				key: "AIzaSyBq9uvAd3KJMszF-1EAVvikMJd9NmcTAYs",
+				part: "snippet",
+				maxResults: 10,
+				order: "viewCount",
+				q: keyword,
+				type: "video",
+				videoEmbeddable: true
+
+			};
+
+			$http({
+
+				method: 'GET',
+
+				url:url,
+
+				params: request
+
+			}).
+
+			success(function(data){
+
+				$scope.results = data.items;
+
+			}).
+
+			error(function(){
+
+				alert('error');
+
+			});
+
+		};
 
 	});
